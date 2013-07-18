working dt, fixed the masking problem

TreeTest.py
@@ -5,16 +5,21 @@ from math import pi
 
 dt = Tree()
 
-a = np.random.normal(1, 1, 10).reshape(-1,1)
-b = np.random.normal(1, 1.5, 10).reshape(-1,1)
+samples = 50000
+
+a = np.random.normal(1, 1, samples).reshape(-1,1)
+b = np.random.normal(1, 1.5, samples).reshape(-1,1)
 signalSample     = np.hstack((a,b))
 
-c = np.random.normal(0, 1, 100).reshape(-1,1)
-d = np.random.normal(0, 1.5, 100).reshape(-1,1)
+c = np.random.normal(0, 1, samples).reshape(-1,1)
+d = np.random.normal(0, 1.5, samples).reshape(-1,1)
 backgroundSample     = np.hstack((c,d))
 
 dt.load(signalSample,backgroundSample)
 dt.build()
 
-test = np.array((1.,1.))
-print dt.classify(test)
+dt.draw()
+test_sig = np.array((1.,1.))
+print dt.classify(test_sig)
+test_bkg = np.array((0.,0.))
+print dt.classify(test_bkg)

bdtpy/tree.py
@@ -13,9 +13,9 @@ class Tree():
         Create a Decision Tree
         """
         # Paramaters
-        self.maxNodes = 4
+        self.maxNodes = 10000
         self.nCuts = 20
-        self.nEventsMin = 20
+        self.nEventsMin = -1
         self.rootNode = None
 
         # Counting stuff
@@ -33,7 +33,8 @@ class Tree():
         self.nSig = sigData.shape[0]
         self.nBkg = bkgData.shape[0]
 
-        #self.nEventsMin = max(20,(self.nSig+self.nBkg)/(self.nVars*self.nVars*10))
+        if self.nEventsMin<=0 :
+            self.nEventsMin = max(20,(self.nSig+self.nBkg)/(self.nVars*self.nVars*10))
 
     def build(self,node=None,sigMask=None,bkgMask=None):
         """
@@ -53,30 +54,32 @@ class Tree():
         if bkgMask == None :
             bkgMask = np.ones(self.nBkg, dtype=bool)
         
-        print sigMask
-
         # check parameters, this may be a leaf...
         leafNode = False
         if self.nNodes >= self.maxNodes :
             leafNode = True
 
-        #sigEvents = self.sigData[sigMask].shape[0]
-        #bkgEvents = self.bkgData[sigMask].shape[0]
 
-        sigEvents = np.bincount(sigMask,minlength=2)[1]
-        bkgEvents = np.bincount(bkgMask,minlength=2)[1]
-
+        sigEvents = long(np.sum(sigMask))
+        bkgEvents = long(np.sum(bkgMask))
+        
+        #print "~~~~"
+        #print sigMask
         #print sigEvents,bkgEvents
 
-        if sigEvents + bkgEvents < self.nEventsMin:
+        if sigEvents + bkgEvents < 2*self.nEventsMin:
+            leafNode = True
+        if sigEvents<=0 or  bkgEvents<=0:
             leafNode = True
 
         if leafNode :
             node.leaf = True
             if float(sigEvents)/(sigEvents+bkgEvents) > 0.5:
-                node.retVal = 1.
+                node.retVal = 1
+                #print "signal"
             else :
-                node.retVal = 0.
+                node.retVal = 0
+                #print "background"
             return 
 
         bestGini=-1.
@@ -86,6 +89,8 @@ class Tree():
         parentIndex = weightedGini(sigEvents,bkgEvents)
         #print parentIndex
         for var in xrange(self.nVars) :
+            #print "var",var
+
             histRange = (
                 min(self.bkgData[bkgMask,var].min(),self.sigData[sigMask,var].min()),
                 max(self.bkgData[bkgMask,var].max(),self.sigData[sigMask,var].max())
@@ -97,14 +102,14 @@ class Tree():
             sigHist, bins = np.histogram(self.sigData[sigMask,var],bins)
             sigYield = np.cumsum(sigHist, dtype=float)
 
-            # print "~~~~"
-            # print bins
+            # 
+            #print type(sigMask)
 
-            # print sigHist
-            # print bkgHist
+            #print sigHist
+            #print bkgHist
 
-            # print sigYield
-            # print bkgYield
+            #print sigYield
+            #print bkgYield
 
 
             #rightIndex = gini(sigYield,bkgYield)
@@ -114,17 +119,16 @@ class Tree():
             rightIndex = vecWeightedGini(sigEvents-sigYield,bkgEvents-bkgYield)
             
             diff = (parentIndex - leftIndex - rightIndex)/(sigEvents+bkgEvents)
-            # print diff
+            #print diff
             maxInd = diff[:-1].argmax()
-            if diff[maxInd] > bestGini :
+            if diff[maxInd] >= bestGini  : #
                 bestGini = diff[maxInd]
                 bestVar = var
-                bestCutVal = bins[maxInd+1] # +1? FIXME
+                bestCutVal = bins[maxInd+1] 
 
         node.setCuts(bestVar,bestCutVal)
-        #node.leaf = True # to fix
 
-        #print bestCutVal
+        #print bestVar,bestCutVal
 
         self.nNodes +=2
 
@@ -136,20 +140,32 @@ class Tree():
         rightnode.parent = node
         node.right = rightnode
 
-        sigMaskLeft = np.array(sigMask,copy=True)
-        sigMaskLeft = [(sigMask[i] and self.sigData[i,bestVar] < bestCutVal) for i in xrange(self.nSig)]
-        bkgMaskLeft = np.array(bkgMask,copy=True)
-        bkgMaskLeft = [(bkgMask[i] and self.bkgData[i,bestVar] < bestCutVal) for i in xrange(self.nBkg)]
-        self.build(node=leftnode,sigMask=sigMaskLeft,bkgMask=bkgMaskLeft)
+        # Problem! the masks are initialised correctly, passed a first time correctly, but on the second
+        # time they become python lists I think (I'm an idiot and doing the inline shit)
+        sigMaskLeft = np.copy(sigMask)
+        sigMaskRight = np.copy(sigMask)
+        for i in xrange(self.nSig):
+            if sigMask[i] :
+                sigMaskLeft[i] = self.sigData[i,bestVar] < bestCutVal
+                sigMaskRight[i] = ~sigMaskLeft[i]
 
-        sigMaskRight = np.array(sigMask,copy=True)
-        sigMaskRight = [(sigMask[i] and self.sigData[i,bestVar] >= bestCutVal) for i in xrange(self.nSig)]
-        bkgMaskRight = np.array(bkgMask,copy=True)
-        bkgMaskRight = [(bkgMask[i] and self.bkgData[i,bestVar] >= bestCutVal) for i in xrange(self.nBkg)]
+        bkgMaskLeft = np.copy(bkgMask)
+        bkgMaskRight = np.copy(bkgMask)
+        for i in xrange(self.nSig):
+            if bkgMask[i] :
+                bkgMaskLeft[i] = self.bkgData[i,bestVar] < bestCutVal
+                bkgMaskRight[i] = ~bkgMaskLeft[i]
+        
+        self.build(node=leftnode,sigMask=sigMaskLeft,bkgMask=bkgMaskLeft)
         self.build(node=rightnode,sigMask=sigMaskRight,bkgMask=bkgMaskRight)
 
         return 
 
     def classify(self, event):
         return self.rootNode.classify(event)
+
+    def draw(self):
+        print "Printing Tree:"
+        self.rootNode.draw()
+        return 
 