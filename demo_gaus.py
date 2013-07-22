6d4ea91250cb49cf560d0fa98a8440482c3b1eab
Martyn Jarvis <martynjarvis@gmail.com>
2013-07-22 17:05:49 +0000

    cleaned up demo gauss example

--- demo_gaus.py
+++ demo_gaus.py
@@ -4,31 +4,33 @@ from math import pi
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+# BDT example. Two 2d gaussians, signal centred at +1,+1 and background centred at -1,-1
+
+events = 1000
+
+# Create BDT
 bdt = BDT()
 
+# create and load training samples
+signal = np.random.normal(1, 1.5, 2*events).reshape(-1,2)
+background = np.random.normal(-1, 1.5, 2*events).reshape(-1,2)
+bdt.load(signal,background)
+
+# Train
+bdt.build()
+
+# show plot of BDT output against x,y
+delta = 0.05
+x = y = np.arange(-3.0, 3.0, delta)
+X, Y = np.meshgrid(x, y)
+
+# TODO a vectorised version of the classify fn should be in the BDT class
 def f(x,y):
     return bdt.classify(np.array((x,y)))
 vecF = np.vectorize(f)
 
-samples = 1000
-
-x1 = np.random.normal(1, 2, samples).reshape(-1,1)
-y1 = np.random.normal(1, 2, samples).reshape(-1,1)
-signalSample     = np.hstack((x1,y1))
-x2 = np.random.normal(-1, 2, samples).reshape(-1,1)
-y2 = np.random.normal(-1, 2, samples).reshape(-1,1)
-backgroundSample     = np.hstack((x2,y2))
-
-bdt.load(signalSample,backgroundSample)
-bdt.build()
-
-delta = 0.05
-x = y = np.arange(-3.0, 3.0, delta)
-X, Y = np.meshgrid(x, y)
 Z = vecF(X,Y)
-
 im = plt.imshow(Z,origin='lower', cmap='bone',extent=[-3,3,-3,3])
-# interpolation='bilinear',
 plt.colorbar(shrink=.92)
 plt.show()
 
